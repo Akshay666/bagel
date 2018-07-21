4ff37c67e6c08e6499fabdb6ef272d0947b66bb7
Bryan Chen <bryan.chen@glassdoor.com>
2018-07-21 10:22:22 -0700

    minimum search for people

--- node-frontend/bagel-bot/search.js
+++ node-frontend/bagel-bot/search.js
@@ -1,5 +1,14 @@
 const search = ((req,res) => {
-    res.send({text: `looks like you typed ${req.body.text}`});
+    let data = require("./yoyo.json");
+    let helper = "someone not on this team....";
+    Object.keys(data).forEach((user) => {
+        data[user].n_sorted_words.forEach((obj)=> {
+            if(Object.keys(obj)[0] === req.body.text){
+                helper = user;
+            }
+        })
+    })
+    res.send({text: `I think you should contact ${helper}!`});
 });
 
 module.exports = search;
