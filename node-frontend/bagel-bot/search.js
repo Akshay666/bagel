bfbf2455e28c41a5a06d23d909f47b521294eb23
Bryan Chen <bryan.chen@glassdoor.com>
2018-07-21 15:19:32 -0700

    big changes

--- node-frontend/bagel-bot/search.js
+++ node-frontend/bagel-bot/search.js
@@ -1,4 +1,4 @@
-var fetch = require('node-fetch');
+const fetch = require('node-fetch');
 
 const query = (searchText, response_url) => {
     fetch("http://d53bf794.ngrok.io/full_data_raw")
@@ -10,25 +10,31 @@ const query = (searchText, response_url) => {
                 users[user].n_sorted_words.forEach((wordObj) => {
                     let word = Object.keys(wordObj)[0];
                     if (word === searchText){
-                        experienced.push({user: user, score: wordObj.score, img: users[user].info.profile.image_original, title: users[user].info.profile.title});
+                        experienced.push({user: user, score: wordObj.score, img: users[user].info.profile.image_original, title: users[user].info.profile.real_name});
                     }
                 })
             });
             let response = {
+                "text": "*People*",
                 "attachments": []
             };
             if(experienced.length) {
-                const maxLen = 5;
+                const maxPeople = 3;
+                const maxSkills = 5;
+                const maxComment = 2;
                 experienced.sort((a, b)=> a.score > b.score);
-                experienced = experienced.slice(0,maxLen);
+                experienced = experienced.slice(0,maxPeople);
                 experienced.forEach(userObj => {
                     let info = {
-                        "title": `<@${userObj.user}>${userObj.title ? ", " + userObj.title : ""}`,
-                        "text": `frequently mentions ${users[userObj.user].n_sorted_words.slice(0,maxLen).map((skillObj) => {
+                        "title": `<@${userObj.user}>${userObj.title ? ", " + userObj.title: ""}`,
+                        "text": `_frequent mentions:_ ${users[userObj.user].n_sorted_words.slice(0,maxSkills).map((skillObj) => {
                             let mention = Object.keys(skillObj)[0];
-                            return mention === searchText ? `*${mention}*` : mention
-                        }).join(", ")
-                            }`,
+                            return mention === searchText ? `*${mention}` : mention;
+                        }).join(", ")} ${users[userObj.user].m_sorted_comments.length ? "\n*Top Comments*" : ""}`,
+                        "fields": users[userObj.user].m_sorted_comments.slice(0,maxComment).map((commentObj)=> {
+                            let comment = Object.keys(commentObj)[0];
+                            return {value:`[${commentObj[comment]} :thumbsup:] ${comment.slice(0,100).replace(/\n|\r/g, "")}`}
+                        }),
                         "thumb_url": userObj.img,
                         "mrkdwn_in": [
                             "text"
@@ -36,7 +42,6 @@ const query = (searchText, response_url) => {
                     };
                     response.attachments.push(info);
                 });
-                response.text = ""
             }
             else {
                 response.text = `Nothing found, sorry :(`
