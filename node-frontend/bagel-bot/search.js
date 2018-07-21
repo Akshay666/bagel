f10b969463efe92fcd0fae2aa6faba80f37b97a3
Bryan Chen <bryan.chen@glassdoor.com>
2018-07-21 14:19:24 -0700

    Null check

--- node-frontend/bagel-bot/search.js
+++ node-frontend/bagel-bot/search.js
@@ -10,7 +10,7 @@ const query = (searchText, response_url) => {
                 users[user].n_sorted_words.forEach((wordObj) => {
                     let word = Object.keys(wordObj)[0];
                     if (word === searchText){
-                        experienced.push({user: user, score: wordObj.score});
+                        experienced.push({user: user, score: wordObj.score, img: users[user].info.profile.image_original, title: users[user].info.profile.title});
                     }
                 })
             });
@@ -18,17 +18,18 @@ const query = (searchText, response_url) => {
                 "attachments": []
             };
             if(experienced.length) {
-                const maxLen = 10;
+                const maxLen = 5;
                 experienced.sort((a, b)=> a.score > b.score);
                 experienced = experienced.slice(0,maxLen);
                 experienced.forEach(userObj => {
                     let info = {
-                        "title": `<@${userObj.user}>`,
+                        "title": `<@${userObj.user}>${userObj.title ? ", " + userObj.title : ""}`,
                         "text": `frequently mentions ${users[userObj.user].n_sorted_words.slice(0,maxLen).map((skillObj) => {
                             let mention = Object.keys(skillObj)[0];
                             return mention === searchText ? `*${mention}*` : mention
                         }).join(", ")
                             }`,
+                        "thumb_url": userObj.img,
                         "mrkdwn_in": [
                             "text"
                         ]
@@ -51,8 +52,13 @@ const query = (searchText, response_url) => {
         });
 };
 const search = ((req, res) => {
-    query(req.body.text, req.body.response_url);
-    res.send({text: `You searched for *${req.body.text}*, we think these people/channels could help:`});
+    if(!req.body.text){
+        res.send({text: "You didn't search for anything!"});
+    }
+    else {
+        query(req.body.text, req.body.response_url);
+        res.send({text: `:tada: You searched for *${req.body.text}*, we think these people/channels could help:`});
+    }
 
 });
 
