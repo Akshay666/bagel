ce2f6157f281fafa62b5908111cc72739dfff0b0
Grace Lam <gracelam@mit.edu>
2018-07-21 13:19:05 -0700

    add prior frequencies

--- fetch.py
+++ fetch.py
@@ -1,11 +1,13 @@
 import data
 import create_index
 import sort
+import priors
 
 def fetch():
 	messages = data.get()
 	index = create_index.create_index(messages)
-	output = sort.sort(*index)
+	prior = priors.get_prior("prior_word_freq.txt")
+	output = sort.sort(*index, prior_freq=prior)
 	return output
 
 if __name__ == "__main__":
