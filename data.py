ddd8cc980de8129d98da39210305d4f43023113c
Luke Shimanuki <luke.shimanuki@gmail.com>
2018-07-21 09:14:52 -0700

    add data fetching

---
+++ data.py
@@ -0,0 +1,42 @@
+from slackclient import SlackClient
+import pdb
+import os
+
+# Create a SlackClient for your bot to use for Web API requests
+SLACK_FETCH_TOKEN = os.environ["SLACK_FETCH_TOKEN"]
+CLIENT = SlackClient(SLACK_FETCH_TOKEN)
+
+DEBUG = False
+
+def rprint(value):
+	if DEBUG:
+		print(value)
+	return value
+
+def get():
+	channels = [
+		channel['id']
+		for channel in
+		CLIENT.api_call('conversations.list', limit=1000, type='public_channel,private_channel,im,mpim')['channels']
+	]
+
+	messages = [
+		rprint((message['user'], channel, message['text'], sum(
+			reaction['count']
+			for reaction in message['reactions']
+		) if 'reactions' in message else 0))
+		for channel in channels
+		for message in
+		CLIENT.api_call('conversations.history', channel=channel, limit=1000)['messages']
+		if 'user' in message and 'text' in message
+	]
+
+	return messages
+
+if __name__ == "__main__":
+	messages = get()
+	print('\n'.join(
+		str(message)
+		for message in messages
+	))
+
