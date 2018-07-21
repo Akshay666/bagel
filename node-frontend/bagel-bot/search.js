6d2463b58a0a4a5c77df25933df1c78dd0f24675
Erica Weng <eweng@pinterest.com>
2018-07-21 14:15:10 -0700

    fix

--- node-frontend/bagel-bot/search.js
+++ node-frontend/bagel-bot/search.js
@@ -16,42 +16,95 @@ const query = (searchText, response_url) => {
                 usernames.forEach((username) => {
                     users[username].n_sorted_words.forEach((wordObj) => {
                         let word = Object.keys(wordObj)[0];
-                        if (word === queryWord){
+                        if (word === queryWord) {
                             experienced.push({user: username, score: wordObj.score});
                         }
                     });
-            let response = {
-                "attachments": []
-            };
-            if(experienced.length) {
-                const maxLen = 10;
-                experienced.sort((a, b) => a.score > b.score);
-                experienced = experienced.slice(0,maxLen);
-                experienced.forEach(userObj => {
-                    let info = {
-                        "title": `<@${userObj.user}>`,
-                        "text": `frequently mentions ${users[userObj.user].n_sorted_words.slice(0,maxLen).map((skillObj) => {
-                            let mention = Object.keys(skillObj)[0];
-                            return mention === req.body.text ? `*${mention}*` : mention 
-                            }).join(", ")
-                        }`,
-                        "mrkdwn_in": [
-                            "text"
-                        ]
+                    let response = {
+                        "attachments": []
                     };
-                    response.attachments.push(info);
+                    if (experienced.length) {
+                        const maxLen = 10;
+                        experienced.sort((a, b) => a.score > b.score);
+                        experienced = experienced.slice(0, maxLen);
+                        experienced.forEach(userObj => {
+                            let info = {
+                                "title": `<@${userObj.user}>`,
+                                "text": `frequently mentions ${users[userObj.user].n_sorted_words.slice(0, maxLen).map((skillObj) => {
+                                    let mention = Object.keys(skillObj)[0];
+                                    return mention === req.body.text ? `*${mention}*` : mention
+                                }).join(", ")
+                                    }`,
+                                "mrkdwn_in": [
+                                    "text"
+                                ]
+                            };
+                            response.attachments.push(info);
+                        });
+                        let response = {
+                            "attachments": []
+                        };
+                        if (experienced.length) {
+                            const maxLen = 10;
+                            experienced.sort((a, b) => a.score > b.score);
+                            experienced = experienced.slice(0, maxLen);
+                            experienced.forEach(userObj => {
+                                let info = {
+                                    "title": `<@${userObj.user}>`,
+                                    "text": `frequently mentions ${users[userObj.user].n_sorted_words.slice(0, maxLen).map((skillObj) => {
+                                        let mention = Object.keys(skillObj)[0];
+                                        return mention === searchText ? `*${mention}*` : mention
+                                    }).join(", ")
+                                        }`,
+                                    "mrkdwn_in": [
+                                        "text"
+                                    ]
+                                };
+                                response.attachments.push(info);
+                            });
+                            response.text = ""
+                        }
+                        else {
+                            response.text = `Nothing found, sorry :(`
+                        }
+
+                        fetch(response_url, {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                            body: JSON.stringify(response),
+                        })
+                            .catch(console.err);
+
+                        getChannels();
+                    }
+                });
+            }
+
+            const getChannels = (data, responseUrl) => {
+                let users = data.users;
+                let experienced = []; // users who have used this queryWord frequently
+                let channelIds = Object.keys(channels);
+                channelIds.forEach((channelId) => {
+                    users[channelId].n_sorted_words.forEach((wordObj) => {
+                        let word = Object.keys(wordObj)[0];
+                        if (word === queryWord) {
+                            experienced.push({channel: channelId, score: wordObj.score});
+                        }
+                    })
                 });
                 let response = {
                     "attachments": []
                 };
                 if (experienced.length) {
-                    const maxLen = 10;
+                    const maxLen = 3;
                     experienced.sort((a, b) => a.score > b.score);
-                    experienced = experienced.slice(0,maxLen);
-                    experienced.forEach(userObj => {
+                    experienced = experienced.slice(0, maxLen);
+                    experienced.forEach(channelObj => {
                         let info = {
-                            "title": `<@${userObj.user}>`,
-                            "text": `frequently mentions ${users[userObj.user].n_sorted_words.slice(0,maxLen).map((skillObj) => {
+                            "title": `<#${channelObj.channel}>`,
+                            "text": `frequently mentions ${users[userObj.user].n_sorted_words.slice(0, maxLen).map((skillObj) => {
                                 let mention = Object.keys(skillObj)[0];
                                 return mention === searchText ? `*${mention}*` : mention
                             }).join(", ")
@@ -68,7 +121,7 @@ const query = (searchText, response_url) => {
                     response.text = `Nothing found, sorry :(`
                 }
 
-                fetch(response_url, {
+                fetch(responseUrl, {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json",
@@ -76,60 +129,8 @@ const query = (searchText, response_url) => {
                     body: JSON.stringify(response),
                 })
                     .catch(console.err);
-
-                getChannels();
-            }
-        });
-}
-
-const getChannels = (data, responseUrl) => {
-    let users = data.users;
-    let experienced = []; // users who have used this queryWord frequently
-    let channelIds = Object.keys(channels);
-    channelIds.forEach((channelId) => {
-        users[channelId].n_sorted_words.forEach((wordObj) => {
-            let word = Object.keys(wordObj)[0];
-            if (word === queryWord){
-                experienced.push({channel: channelId, score: wordObj.score});
-            }
-        })
-    });
-    let response = {
-        "attachments": []
-    };
-    if (experienced.length) {
-        const maxLen = 3;
-        experienced.sort((a, b) => a.score > b.score);
-        experienced = experienced.slice(0,maxLen);
-        experienced.forEach(channelObj => {
-            let info = {
-                "title": `<#${channelObj.channel}>`,
-                "text": `frequently mentions ${users[userObj.user].n_sorted_words.slice(0,maxLen).map((skillObj) => {
-                    let mention = Object.keys(skillObj)[0];
-                    return mention === searchText ? `*${mention}*` : mention
-                }).join(", ")
-                    }`,
-                "mrkdwn_in": [
-                    "text"
-                ]
             };
-            response.attachments.push(info);
-        });
-        response.text = ""
-    }
-    else {
-        response.text = `Nothing found, sorry :(`
-    }
-
-    fetch(responseUrl, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(response),
-    })
-        .catch(console.err);
-}
+        }
 
 const search = ((req, res) => {
     query(req.body.text, req.body.response_url);
