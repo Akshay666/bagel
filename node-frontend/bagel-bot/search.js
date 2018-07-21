ebcb5b5d2a33a9c9a33c1e53a27cbaf927d8d5a5
Erica Weng <eweng@pinterest.com>
2018-07-21 11:29:54 -0700

    hi3

--- node-frontend/bagel-bot/search.js
+++ node-frontend/bagel-bot/search.js
@@ -1,30 +1,35 @@
-const search = ((req,res) => {
-    let data = require("./yoyo.json");
-    let experienced = [];
-    Object.keys(data).forEach((user) => {
-        data[user].n_sorted_words.forEach((wordObj) => {
-            let word = Object.keys(wordObj)[0];
-            if(word === req.body.text){
-                experienced.push(user);
-            }
-        })
-    });
+var fetch = require('node-fetch');
 
-    let response = {
-        "text": `You searched for ${req.body.text}, we think these people could help:`,
-        "attachments": []
-    };
-    experienced.forEach(userId => {
-        let info = {
-            "title": `<@${userId}>`,
-            "text": `expert in ${data[userId].n_sorted_words.map((skillObj) => Object.keys(skillObj)[0]).join(", ")}`,
-            "mrkdwn_in": [
-                "text"
-            ]
-        };
-        response.attachments.push(info);
-    });
-    res.send(response);
+const search = ((req, res) => {
+    let data = fetch("http://4d2561bc.ngrok.io/full_data_raw")
+        .then((response) => response.json())
+        .then((data) => {
+            let experienced = [];
+            Object.keys(data).forEach((user) => {
+                data[user].n_sorted_words.forEach((wordObj) => {
+                    let word = Object.keys(wordObj)[0];
+                    if(word === req.body.text){
+                        experienced.push(user);
+                    }
+                })
+            });
+
+            let response = {
+                "text": `You searched for ${req.body.text}, we think these people could help:`,
+                "attachments": []
+            };
+            experienced.forEach(userId => {
+                let info = {
+                    "title": `<@${userId}>`,
+                    "text": `expert in ${data[userId].n_sorted_words.map((skillObj) => Object.keys(skillObj)[0]).join(", ")}`,
+                    "mrkdwn_in": [
+                        "text"
+                    ]
+                };
+                response.attachments.push(info);
+            });
+            res.send(response);
+        });
 });
 
 module.exports = search;
