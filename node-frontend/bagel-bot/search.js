1055c6df89f2f00bbe8c9fdebc8a6cf1683f5449
Bryan Chen <bryan.chen@glassdoor.com>
2018-07-21 13:41:17 -0700

    Immediate response

--- node-frontend/bagel-bot/search.js
+++ node-frontend/bagel-bot/search.js
@@ -1,7 +1,7 @@
 var fetch = require('node-fetch');
 
-const search = ((req, res) => {
-    let data = fetch("http://d53bf794.ngrok.io/full_data_raw")
+const query = (searchText, response_url) => {
+    fetch("http://d53bf794.ngrok.io/full_data_raw")
         .then((response) => response.json())
         .then((data) => {
             let users = data.users;
@@ -9,7 +9,7 @@ const search = ((req, res) => {
             Object.keys(users).forEach((user) => {
                 users[user].n_sorted_words.forEach((wordObj) => {
                     let word = Object.keys(wordObj)[0];
-                    if (word === req.body.text){
+                    if (word === searchText){
                         experienced.push({user: user, score: wordObj.score});
                     }
                 })
@@ -26,22 +26,34 @@ const search = ((req, res) => {
                         "title": `<@${userObj.user}>`,
                         "text": `frequently mentions ${users[userObj.user].n_sorted_words.slice(0,maxLen).map((skillObj) => {
                             let mention = Object.keys(skillObj)[0];
-                            return mention === req.body.text ? `*${mention}*` : mention 
-                            }).join(", ")
-                        }`,
+                            return mention === searchText ? `*${mention}*` : mention
+                        }).join(", ")
+                            }`,
                         "mrkdwn_in": [
                             "text"
                         ]
                     };
                     response.attachments.push(info);
                 });
-                response.text = `You searched for *${req.body.text}*, we think these people could help:`
+                response.text = ""
             }
             else {
-                response.text = `Nobody found for *${req.body.text}*, sorry :(`
+                response.text = `Nothing found, sorry :(`
             }
-            res.send(response);
+            fetch(response_url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(response),
+            })
+                .catch(console.err);
         });
+};
+const search = ((req, res) => {
+    query(req.body.text, req.body.response_url);
+    res.send({text: `You searched for *${req.body.text}*, we think these people/channels could help:`});
+
 });
 
 module.exports = search;
