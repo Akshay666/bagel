bd559aad3bbd1103da73581da8ec81c1724cb5cb
Bryan Chen <bryan.chen@glassdoor.com>
2018-07-21 11:16:32 -0700

    Show users in FE

--- node-frontend/bagel-bot/search.js
+++ node-frontend/bagel-bot/search.js
@@ -10,15 +10,21 @@ const search = ((req,res) => {
         })
     });
 
-    let message_per_user = `${experienced[i]} has experience in {}`
-    let string = "";
-    experienced.forEach(userObj => {
-        let username =  Object.keys(userObj);
-        let string2 = `${username} is good at: `;
-        userObj.n_sorted_words.forEach((word) => string2 += ` - ${word}\n`)
-        string += string2
+    let response = {
+        "text": `You searched for ${req.body.text}, we think these people could help:`,
+        "attachments": []
+    };
+    experienced.forEach(userId => {
+        let info = {
+            "title": `<@${userId}>`,
+            "text": `expert in ${data[userId].n_sorted_words.map((skillObj) => Object.keys(skillObj)[0]).join(", ")}`,
+            "mrkdwn_in": [
+                "text"
+            ]
+        };
+        response.attachments.push(info);
     });
-    res.send({text: string});
+    res.send(response);
 });
 
 module.exports = search;
